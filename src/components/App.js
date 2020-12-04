ff0c05248968d40fedfa23bb6085c20f887ef693
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2020-12-04 12:40:18 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,11 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    const relatives = ["Saranya", "Siva", "Das"];
+    return <div id="main">{/* Do not remove the main div */}</div>;
+  }
 }
-
 
 export default App;
